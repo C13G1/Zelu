443ae1af36059268e603230ed075bae6fa634643
Enzo Ferroni <enzoferroni@hotmail.com>
2026-06-02 17:05:37 -0300

    feat: limit profile and friend name to 10 characters

--- ConexoesAmizaticas/Views/EditFriendProfileView.swift
+++ ConexoesAmizaticas/Views/EditFriendProfileView.swift
@@ -62,6 +62,9 @@ struct EditFriendProfileView: View {
                 .background(.gray.opacity(0.1))
                 .clipShape(RoundedRectangle(cornerRadius: 12))
                 .padding(.horizontal, 40)
+                .onChange(of: viewModel.name) { _, _ in
+                    viewModel.enforceCharacterLimit()
+                }
 
             Spacer()
         }
