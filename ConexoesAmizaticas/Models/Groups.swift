bb3319de71822abf7d39ec9c3fb03fe4612d5f47
Dayo Araujo <dayo.araujosz@gmail.com>
2026-06-15 15:04:22 -0300

    fix: file name

--- ConexoesAmizaticas/Models/Groups.swift
+++ ConexoesAmizaticas/Models/Groups.swift
@@ -1,5 +1,5 @@
 //
-//  Grups.swift
+//  Groups.swift
 //  ConexoesAmizaticas
 //
 //  Created by Jonas Fernando Nascimento Melo on 12/06/26.
@@ -8,7 +8,7 @@ import Foundation
 import SwiftData
 
 @Model
-class Grups {
+class Groups {
     var name: String
     var image: Data
     var connections: [Connection]
